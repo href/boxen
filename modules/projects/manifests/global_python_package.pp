4faadd248758f2b783ab04883845b8dbd88dd435
Denis Krienbühl <denis@href.ch>
2014-12-22 12:17:41 +0100

    Make pip install case independent

--- modules/projects/manifests/global_python_package.pp
+++ modules/projects/manifests/global_python_package.pp
@@ -1,9 +1,9 @@
 # installs a python package globally into python2 *and* python 3
 define projects::global_python_package() {
     exec { "pip2 install ${name}" :
-        unless => "pip2 freeze list | grep ${name} --silent"
+        unless => "pip2 freeze list | grep ${name} --silent --ignore-case"
     }
     exec { "pip3 install ${name}" :
-        unless => "pip3 freeze list | grep ${name} --silent"
+        unless => "pip3 freeze list | grep ${name} --silent --ignore-case"
     }
 }
